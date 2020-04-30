89916eacaa180469066f415eb7aa204e86e0510c
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2020-04-30 19:47:25 +0530

    login router created

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const { generateHash, compareHash } = require("../utils/hash");
+const { createToken, validateToken } = require("../utils/loginTokenManager")
 
 const userRouter = express.Router();
 
@@ -19,14 +20,26 @@ userRouter
   })
 
   .post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email }).exec();
-    if (user) {
-      const result = await compareHash(password, user.password);
-      if (result) {
+    try{
+      const { email, password } = req.body;
+      const user = await User.findOne({ email }).exec();
+      console.log(user);
+      if (user) {
+        const result = await compareHash(password, user.password);
+        if (result) {
+          const jwtToken = createToken({email});
+          res.cookie("jwt", jwtToken);
+          res.status(200).json({status: "SUCCESS", jwtToken});
+        }else{
+          res.status(400).send("Invalid Request");
+        }
+      }else{
+        res.status(400).send("Invalid Request");
       }
+    }catch(e){
+      console.error(e);
+      res.status(500).send("Internal Server Error")
     }
-  })
-  ;
+  });
 
 module.exports = userRouter;
